48554f0c89a9198c273f6d5a37b106577743d8d1
JoeyFan <1834879843@qq.com>
2025-08-28 22:16:28 +0800

    初始化基本项目配置，基于skelon搭建基础开发骨架

---
+++ postcss.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8",
+      ],
+    }),
+
+    // 宽度和横向间距 dw → vw
+    require('postcss-px-to-viewport-8-plugin')({
+      viewportWidth: 1440,
+      unitToConvert: 'dw',
+      unitPrecision: 3,
+      viewportUnit: "vw", 
+      minPixelValue: 1,
+      mediaQuery: false,
+    }),
+
+    // 其余px单位转换为rem
+    require('postcss-pxtorem')({
+      rootValue: 10,
+      unitPrecision: 3,
+      unitToConvert: 'px',
+      propList: ['*'],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0,
+    }),
+  ],
+};
---
+++ vite.config.js
@@ -0,0 +1,57 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import postcss from './postcss.config.js';
+
+function getEntries(type,pattern){
+    const glob = require('glob');
+    const entries = {};
+
+    const options = {};
+    if(type != 'js'){
+        options.ignore = [
+            'src/components/**'
+        ]
+    }
+
+    const files = glob.sync(`${pattern}.${type}`,options);
+
+    files.forEach(file => {
+        const name = path.basename(file, path.extname(file));
+        entries[name] = path.resolve(__dirname,file);
+    });
+
+    return entries;
+}
+
+
+export default defineConfig({
+    build:{
+        outDir: 'assets',
+        emptyOutDir: false,
+        rollupOptions:{
+            input:{
+                ...getEntries('js','src/components/*'),
+                ...getEntries('{css,scss}','src/**/*')
+            },
+            output:{
+                entryFileNames: "[name].js",
+                assetFileNames: "[name].[ext]"
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+                    @use "@/components/styles/_variables.scss" as *;
+                `
+            },
+        },
+        postcss,
+    },
+});
